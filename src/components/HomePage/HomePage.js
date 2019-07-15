614145d6e4cf8ce304061fb0b41d1463baa42412
Jacqueline Montes-De Leon <jmontesd@ucsc.edu>
2019-07-15 14:36:21 -0700

    adding sidebar UI

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -12,12 +12,63 @@ const HomePage = (props) => {
   if (!isLoggedIn) return <Redirect to="/login" />;
 
   return (
-    <div className="container">
+    <div class="container section">
+        <ul class="sidenav sidenav-fixed" id="menu-side">
+          <li>
+            <div class="user-view">
+              <div class="background">
+                <img src="https://bodhispiritualcenter.org/wp-content/uploads/2014/10/redwoods-looking-up-650x487.jpg" class="centered"/>
+              </div>
+              <a href="#user">
+                <img
+                  src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/5536321/original/stickfigure/make-a-hand-drawn-stick-figure-portrait.png"
+                  class="circle"
+                />
+              </a>
+              <a href="#name">
+                {/* <span class="name white"> Jacqueline Montes</span> */}
+                <span class="center-align name white">Jacqueline Montes</span>
+                <p>
+
+                </p>
+              </a>
+              <a href="#email">
+                <span class="center-align name white"> jmontesd@ucsc.edu</span>
+              </a>
+            </div>
+            <div className="div"></div>
+          </li>
+          <ul class="collapsible">
+            <li>
+              <div class="collapsible-header">
+                <i class="material-icons">home</i>First
+              </div>
+              <div class="collapsible-body">
+                <p>Found Items</p>
+                <p>Lost Items</p>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+                <i class="material-icons">person</i>Second
+              </div>
+              <div class="collapsible-body">
+                <span>Lorem ipsum dolor sit amet.</span>
+              </div>
+            </li>
+          </ul>
+        </ul>
+      
+  
+
+    <div className="container" id="body">
       {posts &&
         posts
           .slice()
           .sort((a, b) => b.createdAt - a.createdAt)
           .map((post) => <Item key={post.id} post={post} />)}
+    </div>
+
     </div>
   );
 };
