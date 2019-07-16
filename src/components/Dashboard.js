5eb9a8959b608db3f522554be6ade226ed61f63a
Cesar <48188267+cesar93683@users.noreply.github.com>
2019-07-15 17:49:55 -0700

    fix collapse

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,61 +1,61 @@
-import React, { Component } from "react";
-import LostItem from "../items/LostItem";
-import FoundItem from "../items/FoundItem";
+// import React, { Component } from "react";
+// import LostItem from "../items/LostItem";
+// import FoundItem from "../items/FoundItem";
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div class="container section">
-        <ul class="sidenav sidenav-fixed" id="menu-side">
-          <li>
-            <div class="user-view">
-              <div class="background">
-                <img src="https://i.pinimg.com/736x/bc/c1/80/bcc1803e1c59e1443bd492acfafa5dbc.jpg" />
-              </div>
-              <a href="#user">
-                <img
-                  src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/5536321/original/stickfigure/make-a-hand-drawn-stick-figure-portrait.png"
-                  class="circle"
-                />
-              </a>
-              <a href="#name">
-                <span class="name black-text"> Jacqueline Montes</span>
-              </a>
-              <a href="#email">
-                <span class="name black-text"> jmontesd@ucsc.edu</span>
-              </a>
-            </div>
-          </li>
-          <ul class="collapsible">
-            <li>
-              <div class="collapsible-header">
-                <i class="material-icons">home</i>HomePage
-                <i class="material-icons right" style={{ marginRight: -10 }}>
-                  arrow_drop_down
-                </i>
-              </div>
-              <div class="collapsible-body">
-                <p>Found Items</p>
-                <p>Lost Items</p>
-              </div>
-            </li>
-            <li>
-              <div class="collapsible-header">
-                <i class="material-icons">person</i>My Profile
-                <i class="material-icons right" style={{ marginRight: -10 }}>
-                  arrow_drop_down
-                </i>
-              </div>
-              <div class="collapsible-body">
-                <p>Posts</p>
-                <p>Messages</p>
-              </div>
-            </li>
-          </ul>
-        </ul>
-      </div>
-    );
-  }
-}
+// class Dashboard extends Component {
+//   render() {
+//     return (
+//       <div class="container section">
+//         <ul class="sidenav sidenav-fixed" id="menu-side">
+//           <li>
+//             <div class="user-view">
+//               <div class="background">
+//                 <img src="https://i.pinimg.com/736x/bc/c1/80/bcc1803e1c59e1443bd492acfafa5dbc.jpg" />
+//               </div>
+//               <a href="#user">
+//                 <img
+//                   src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/5536321/original/stickfigure/make-a-hand-drawn-stick-figure-portrait.png"
+//                   class="circle"
+//                 />
+//               </a>
+//               <a href="#name">
+//                 <span class="name black-text"> Jacqueline Montes</span>
+//               </a>
+//               <a href="#email">
+//                 <span class="name black-text"> jmontesd@ucsc.edu</span>
+//               </a>
+//             </div>
+//           </li>
+//           <ul class="collapsible">
+//             <li>
+//               <div class="collapsible-header">
+//                 <i class="material-icons">home</i>HomePage
+//                 <i class="material-icons right" style={{ marginRight: -10 }}>
+//                   arrow_drop_down
+//                 </i>
+//               </div>
+//               <div class="collapsible-body">
+//                 <p>Found Items</p>
+//                 <p>Lost Items</p>
+//               </div>
+//             </li>
+//             <li>
+//               <div class="collapsible-header">
+//                 <i class="material-icons">person</i>My Profile
+//                 <i class="material-icons right" style={{ marginRight: -10 }}>
+//                   arrow_drop_down
+//                 </i>
+//               </div>
+//               <div class="collapsible-body">
+//                 <p>Posts</p>
+//                 <p>Messages</p>
+//               </div>
+//             </li>
+//           </ul>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-export default Dashboard;
+// export default Dashboard;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,74 +4,94 @@ import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import M from 'materialize-css';
 import Item from '../Item/Item';
 
-const HomePage = (props) => {
-  const { posts, isLoggedIn } = props;
+class HomePage extends React.Component {
+  // get a reference to the element after the component has mounted
+  componentDidMount() {
+    M.Sidenav.init(this.sidenav);
+    M.Collapsible.init(this.collapsible);
+  }
 
-  if (!isLoggedIn) return <Redirect to="/login" />;
+  render() {
+    const { posts, isLoggedIn } = this.props;
 
-  return (
-    <div class="container section">
-        <ul class="sidenav sidenav-fixed" id="menu-side">
+    if (!isLoggedIn) return <Redirect to="/login" />;
+
+    return (
+      <div className="container section">
+        <ul
+          className="sidenav sidenav-fixed"
+          id="menu-side"
+          ref={(sidenav) => {
+            this.sidenav = sidenav;
+          }}
+        >
           <li>
-            <div class="user-view">
-              <div class="background">
-                <img src="https://bodhispiritualcenter.org/wp-content/uploads/2014/10/redwoods-looking-up-650x487.jpg" class="centered"/>
+            <div className="user-view">
+              <div className="background">
+                <img
+                  alt=""
+                  src="https://bodhispiritualcenter.org/wp-content/uploads/2014/10/redwoods-looking-up-650x487.jpg"
+                  className="centered"
+                />
               </div>
               <a href="#user">
                 <img
+                  alt=""
                   src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/5536321/original/stickfigure/make-a-hand-drawn-stick-figure-portrait.png"
-                  class="circle"
+                  className="circle"
                 />
               </a>
               <a href="#name">
                 {/* <span class="name white"> Jacqueline Montes</span> */}
-                <span class="center-align name white">Jacqueline Montes</span>
-                <p>
-
-                </p>
+                <span className="center-align name white">Jacqueline Montes</span>
+                <p />
               </a>
               <a href="#email">
-                <span class="center-align name white"> jmontesd@ucsc.edu</span>
+                <span className="center-align name white"> jmontesd@ucsc.edu</span>
               </a>
             </div>
-            <div className="div"></div>
+            <div className="div" />
           </li>
-          <ul class="collapsible">
+          <ul
+            className="collapsible"
+            ref={(collapsible) => {
+              this.collapsible = collapsible;
+            }}
+          >
             <li>
-              <div class="collapsible-header">
-                <i class="material-icons">home</i>First
+              <div className="collapsible-header">
+                <i className="material-icons">home</i>First
               </div>
-              <div class="collapsible-body">
+              <div className="collapsible-body">
                 <p>Found Items</p>
                 <p>Lost Items</p>
               </div>
             </li>
             <li>
-              <div class="collapsible-header">
-                <i class="material-icons">person</i>Second
+              <div className="collapsible-header">
+                <i className="material-icons">person</i>Second
               </div>
-              <div class="collapsible-body">
+              <div className="collapsible-body">
                 <span>Lorem ipsum dolor sit amet.</span>
               </div>
             </li>
           </ul>
         </ul>
-      
-  
 
-    <div className="container" id="body">
-      {posts &&
-        posts
-          .slice()
-          .sort((a, b) => b.createdAt - a.createdAt)
-          .map((post) => <Item key={post.id} post={post} />)}
-    </div>
-
-    </div>
-  );
-};
+        <div className="container" id="body">
+          {posts &&
+            posts
+              .slice()
+              .sort((a, b) => b.createdAt - a.createdAt)
+              .map((post) => <Item key={post.id} post={post} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   const { posts } = state.firestore.ordered;
