665008b8d012dfd9e4d9159498ee2842a0d7666b
Gurpreet Singh <gurpreetsingh@eduroam-169-233-169-219.ucsc.edu>
2019-07-15 19:04:16 -0700

    profile design

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -13,12 +13,24 @@ const ProfilePage = (props) => {
 
   return (
     <div className="container">
-      <div>{username}</div>
-      {posts &&
-        posts
-          .slice()
-          .sort((a, b) => b.createdAt - a.createdAt)
-          .map((post) => <Item key={post.id} post={post} />)}
+      <div className="row">
+        <div className="col-8">
+          {posts &&
+            posts
+              .slice()
+              .sort((a, b) => b.createdAt - a.createdAt)
+              .map((post) => <div> <Item key={post.id} post={post} /> </div>)}
+        </div>
+        <div className="col-4" >
+          <div className="profile-wrapper">
+            <img src="https://firebasestorage.googleapis.com/v0/b/slug-and-found.appspot.com/o/images%2Fa12e2b48-2f36-40a2-88ad-8599307a2f14?alt=media&token=d2b68eda-5751-4e03-bca2-ae951a0765e6" className="img-card" alt="" />
+            <div className="card-title">
+              {username}
+            </div>
+            <a className="btn btn-warning btn-card"> Message </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
