3319992e7626597ad3f0c3c92bd4a12a3407f5b1
Steven Van Dinh <vandinhsteven@gmail.com>
2019-07-16 18:33:32 -0700

    updated AppRouter for compose page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from '../components/HomePage/HomePage';
 import ItemPage from '../components/ItemPage/ItemPage';
 import MessagesPage from '../components/MessagesPage/MessagesPage';
+import ComposePage from '../components/MessagesPage/ComposePage';
 import NotFoundPage from '../components/NotFoundPage/NotFoundPage';
 import ProfilePage from '../components/ProfilePage/ProfilePage';
 import SubmitPage from '../components/SubmitPage/SubmitPage';
@@ -19,6 +20,7 @@ const AppRouter = () => (
         <Route path="/" component={HomePage} exact />
         <Route path="/item/:id" component={ItemPage} exact />
         <Route path="/messages" component={MessagesPage} exact />
+        <Route path="/messages/compose" component={ComposePage} exact />
         <Route path="/profile/:id" component={ProfilePage} exact />
         <Route path="/submit" component={SubmitPage} exact />
         <Route path="/login" component={LogInPage} exact />
