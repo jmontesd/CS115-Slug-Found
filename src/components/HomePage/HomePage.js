c5b9a2e19e119ad4264644f6f9286692ce5de697
Cesar <48188267+cesar93683@users.noreply.github.com>
2019-07-19 18:51:52 -0700

    fix bug

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,8 +3,9 @@ import './HomePage.scss';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import M from 'materialize-css';
+import SearchBox from '../SearchBox/SearchBox';
 import Item from '../Item/Item';
 
 export class HomePage extends React.Component {
@@ -13,32 +14,30 @@ export class HomePage extends React.Component {
   //   M.Sidenav.init(this.sidenav);
   //   M.Collapsible.init(this.collapsible);
   // }
+  state = { searchItem: '' };
 
   handleInput = (e) => {
-    console.log(e.target.value); 
-    this.setState({ searchItem: e.target.value})
-  }
-  render() {
+    console.log(e.target.value);
+    this.setState({ searchItem: e.target.value });
+  };
 
+  render() {
     const { posts, isLoggedIn } = this.props;
 
     if (!isLoggedIn) return <Redirect to="/login" />;
 
     return (
-        <div className="container" id="body">
-        
-          <SearchBox handleInput={this.handleInput}/>
-          {posts &&
-            posts
-              .slice()
-              .sort((a, b) => b.createdAt - a.createdAt)
-              .filter((post) =>  {
-                return post.title.toLowerCase().includes(this.state.searchItem.toLowerCase());
-              })
-              .map((post) => <Item key={post.id} post={post} />)}
-              
-              
-        </div>
+      <div className="container" id="body">
+        <SearchBox handleInput={this.handleInput} />
+        {posts &&
+          posts
+            .slice()
+            .sort((a, b) => b.createdAt - a.createdAt)
+            .filter((post) => {
+              return post.title.toLowerCase().includes(this.state.searchItem.toLowerCase());
+            })
+            .map((post) => <Item key={post.id} post={post} />)}
+      </div>
     );
   }
 }
